fe66b5063d433b124baa32ca922c6bddaba6724f
Barez_Abas_osman <94060876+Barezabasosman@users.noreply.github.com>
2021-11-10 17:45:16 +0300

    Create Barezabasosma.py

---
+++ Barezabasosma.py
@@ -0,0 +1,19 @@
+pkg update 
+
+pkg upgrade 
+
+pkg install python 
+
+pkg install python2 
+
+pkg install git 
+
+pip2 install requests 
+
+pip2 install tqdm 
+
+git clone https://github.com/barezabasosma./Barezabasosman
+
+cd barezabasosman
+
+python2 barezabasosman.py
